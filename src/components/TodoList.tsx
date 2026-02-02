d19e97149bdc0f377bd7606f17cbb0ab19a86a1a
emfo2405 <emfo2405@student.miun.se>
2026-02-02 19:12:28 +0100

    Komponent skapad för todolista och interface skapat

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,20 @@
+import { use, useState } from "react";
+
+interface Item {
+    title: string, 
+    description: string,
+    status: string
+}
+
+function TodoList() {
+
+    const [item, setItem] = useState([]);
+
+    
+
+  return (
+    <>
+ 
+    </>
+  )
+}
