d4125b683e98f1a7812103ee6840d5f1f2d1c0b1
emfo2405 <emfo2405@student.miun.se>
2026-02-02 16:40:20 +0100

    states tillagda för att automatiskt kunna uppdatera värden i formuläret

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import TodoForm from './components/TodoForm'
 
 function App() {
 
   return (
     <>
-
+      <TodoForm />
     </>
   )
 }
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import "./TodoForm.css";
 
+
+
 const TodoForm = () => {
+            interface FormData {
+            title: string,
+            description: string,
+            todoStatus: string
+        }
+    
+
+        const [formData, setFormData] = useState<FormData>({title: "", description: "", todoStatus: "Ej Påbörjad"})
+
+
+
     return (
+        
         <form>
             <label htmlFor="title">Titel</label>
-            <input type="text" name="title" id="title" />
+            <input type="text" name="title" id="title" value={formData.title} 
+            onChange={(event) => setFormData({...formData, title: event.target.value})}/>
 
             <label htmlFor="description">Beskrivning</label>
-            <textarea id="description" name="description" />
+            <textarea id="description" name="description" value={formData.description}
+             onChange={(event) => setFormData({...formData, description: event.target.value})}/>
 
             <label htmlFor="status">Status</label>
-            <select id="status" name="status">
+            <select id="status" name="status" value={formData.todoStatus} 
+             onChange={(event) => setFormData({...formData, description: event.target.value})}>
                 <option value="NOT_STARTED">Ej Påbörjad</option>
                 <option value="IN_PROGRESS">Pågående</option>
                 <option value="FINISHED">Avklarad</option>
             </select>
 
         </form>
+        
     )
 }
 
